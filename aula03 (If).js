c1bbe14ddaf0f427718e336521242cca5f2c6d20
Yago Mendes <ygmendes21@gmail.com>
2023-06-20 19:00:23 -0300

    Create aula03 (If).js

---
+++ aula03 (If).js
@@ -0,0 +1,35 @@
+const idade = 18;
+const nome = 'Mendes'
+let podeEntrar = false;
+
+if(idade >= 18 && nome === 'Mendes'){
+ console.log('Entrou')
+}
+else{
+    console.log('O usuario não pode entrar !!')
+}
+
+
+/* Explicações DevMendes
+
+if (If é utilizado quando queremos que um bloco de comandos seja executado se uma condição específica for verdadeira.)
+
+>= (Maior/Igual que)
+
+else (else significa algo mais ou algum outro, Ela é utilizada em conjunto com if
+quando vamos apresentar o bloco de comando que deve ser executado se a condição original do if não for verdadeira.)
+
+|| = Quando utilizado com valores booleanos, || retorna verdadeiro caso um dos operandos sejam verdadeiro; se ambos forem falsos, retorna falso.
+
+&& = && retorna verdadeiro caso ambos operandos sejam verdadeiros; caso contrário, retorna falso.
+*/
+
+
+/*
+Diferença de =
+
+Igual (==)
+Estritamente igual (===)
+Não igual (!=)
+*/
+
