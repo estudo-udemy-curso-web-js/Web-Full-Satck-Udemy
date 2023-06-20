236d354ce2668eb743f1b13a3a376fb9270ff5d6
Yago Mendes <ygmendes21@gmail.com>
2023-06-20 20:28:40 -0300

    Create aula05 (While).js

---
+++ aula05 (While).js
@@ -0,0 +1,8 @@
+let idade = 1;
+
+while(idade < 18){
+    console.log('Passou', idade)
+    idade = idade + 1;
+}
+
+// A declaração while cria um laço que executa uma rotina especifica enquanto a condição de teste for avaliada como verdadeira.
