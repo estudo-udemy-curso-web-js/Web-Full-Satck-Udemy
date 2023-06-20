9a7ff01f1a46759f684ff258d69939568df6e03e
Yago Mendes <ygmendes21@gmail.com>
2023-06-20 20:34:05 -0300

    Create aula06 (Do While).js

---
+++ aula06 (Do While).js
@@ -0,0 +1,10 @@
+let idade = 1
+
+do{
+    console.log('Passou');
+    idade = idade + 1;
+} while(idade < 18);
+
+/*
+ declaração do{} while(), cria um laço que executa uma declaração até que o teste da condição for falsa (false).
+ A condição é avaliada depois que o bloco de código é executado, resultando que uma declaração seja executada pelo menos uma vez. */
