85b37f69f1db0a84bf1b6a425b817b9f0c5d8a9b
Yago Mendes <ygmendes21@gmail.com>
2023-06-20 18:40:05 -0300

    att

---
+++ aula02 (Const x Let).js
@@ -0,0 +1,21 @@
+const numero1 = 1;
+let numero2 = 2;
+
+if(numero1 === 1){
+    numero2 = 6
+}
+
+console.log('O Resultado é:', numero2)
+
+
+ 
+/* O erro abaixo demonstra a tentativa de mudança de uma variavel:
+
+const numero1 = 1;
+let numero2 = 2;
+
+if(numero2 === 2){
+    numero1 = 7
+}
+
+console.log('O Resultado é:', numero1)         */
