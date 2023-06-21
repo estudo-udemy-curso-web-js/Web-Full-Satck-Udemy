969b8b37858feb85d8e5cec8896cb80a14fd1546
Yago Mendes <ygmendes21@gmail.com>
2023-06-20 21:50:24 -0300

    att

--- Aula09-1 (Atividade).js
+++ Aula09-1 (Atividade).js
@@ -2,7 +2,7 @@ function calcularLitrosViagem(tempo, velocidadeMedia) {
     const consumoMedio = 12;
     const distancia = tempo * velocidadeMedia;
     const litrosNecessarios = distancia / consumoMedio;
-  
+    
     return litrosNecessarios.toFixed(3);
   }
   
