698c59f95570c83ce09623fd0f745fa21632ec13
Yago Mendes <ygmendes21@gmail.com>
2023-06-20 21:29:23 -0300

    Create Aula09 (Function).js

---
+++ Aula09 (Function).js
@@ -0,0 +1,17 @@
+function soma(item1, item2){
+return item1 + item2;
+}
+
+function subtracao(item1, item2){
+    return item1 - item2;
+}
+
+console.log('A soma é igual a:', soma(5,6));
+console.log('A soma é igual a:', soma(54,62));
+console.log('A subtração é igual a:', subtracao(54,62));
+
+/* A definição da função consiste no uso da palavra chave function (en-US), seguida por:
+
+    - Nome da Função.
+    - Lista de argumentos para a função, entre parênteses e separados por vírgulas.
+    - Declarações JavaScript que definem a função, entre chaves { }. */
